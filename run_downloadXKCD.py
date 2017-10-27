6f964a6d909e875dde3a9c0365c8a1f99cde810a
toonarmycaptain <toonarmycaptain@hotmail.com>
2017-10-27 13:11:34 +0000

    Script to run archiver in background

---
+++ run_downloadXKCD.py
@@ -0,0 +1,8 @@
+#! python3
+"""
+Created on Mon Oct 23 10:20:14 2017
+
+@author: david.antonini // toonarmycaptain
+"""
+import subprocess
+subprocess.Popen(['start', r'C:\Users\david.antonini\XKCD_Archiver\downloadXKCDthreaded.py'], shell=True)
