702f390cbdf8f6727c2248cce584dec6e0d24468
toonarmycaptain <toonarmycaptain@hotmail.com>
2017-11-19 19:02:32 +0000

    Update run_downloadXKCD.py

--- run_downloadXKCD.py
+++ run_downloadXKCD.py
@@ -1,8 +1,9 @@
 #! python3
+# rundownloadXKCD.py - runs downloadXKCD.py 
 """
 Created on Mon Oct 23 10:20:14 2017
 
 @author: david.antonini // toonarmycaptain
 """
 import subprocess
-subprocess.Popen(['start', r'downloadXKCDthreaded.py', shell=True)
+subprocess.Popen(['start', r'downloadXKCD.py'], shell=True)
