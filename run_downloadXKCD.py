75f0d6bd513dd99812eed39f3f94cd4a6671699c
toonarmycaptain <toonarmycaptain@hotmail.com>
2018-05-02 09:12:05 -0500

    Delete run_downloadXKCD.py

--- run_downloadXKCD.py
+++
@@ -1,9 +0,0 @@
-#!./downloadXKCD_env/Scripts/python
-# rundownloadXKCD.py - runs downloadXKCD.py
-"""
-Created on Mon Oct 23 10:20:14 2017
-
-@author: david.antonini // toonarmycaptain
-"""
-import subprocess
-subprocess.Popen(['start', 'downloadXKCD.py'], shell=True)
