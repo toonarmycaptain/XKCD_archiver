b2dedde1ffa1b28490e27e452d1c85adbb64a475
toonarmycaptain <toonarmycaptain@hotmail.com>
2019-10-31 11:11:40 -0500

    Have batch launcher change cwd to script dir. Allows correct running from Start on Windows etc.

--- XKCD_archiver/archive_XKCD.bat
+++ XKCD_archiver/archive_XKCD.bat
@@ -3,6 +3,9 @@
 FOR /F "tokens=* USEBACKQ" %%F IN (`py --version`) DO (SET pyversion=%%F)
 title archive XKCD - %pyversion%
 
+:: Change cwd to script dir.
+cd %~dp0
+
 echo Setting up...
 :: Run dependency install without displaying to console before script:
 .\install_dependencies.bat >nul 2>&1 & echo Done. & py -m downloadXKCD & PAUSE
