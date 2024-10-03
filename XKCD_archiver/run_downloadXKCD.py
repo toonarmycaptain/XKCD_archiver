6e5df92e6ad9190c0dfaf7123733da2397a77afd
toonarmycaptain <toonarmycaptain@hotmail.com>
2024-10-03 09:52:21 -0500

    Use if __name__ == "__main__"

--- XKCD_archiver/run_downloadXKCD.py
+++ XKCD_archiver/run_downloadXKCD.py
@@ -5,5 +5,8 @@ Created on Mon Oct 23 10:20:14 2017
 
 @author: david.antonini // toonarmycaptain
 """
-import subprocess
-subprocess.Popen(['start', 'downloadXKCD.py'], shell=True)
+
+if __name__ == "__main__":
+    import subprocess
+
+    subprocess.Popen(["start", "downloadXKCD.py"], shell=True)
